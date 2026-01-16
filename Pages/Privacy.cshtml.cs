d61bd3503dbd384d64cddc0916e8a0b0468368a3
Nick Lanzo <nlanzo@gmail.com>
2026-01-15 22:09:17 -0600

    initial commit / setup

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinanceApp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
